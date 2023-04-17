74824f0d2e3744449d2dcd4b390dd65b59ebe8cb
Dayael <guerrero.mora.dayael@gmail.com>
2023-04-16 18:07:33 -0600

    Reorganizacion

---
+++ Pagina fsc/build/web/paginas/login_signin.js
@@ -0,0 +1,8 @@
+// const btniniciar = document.getElementById("iniciarses")
+
+// // btniniciar.addEventListener('click', linkpag)
+
+// function linkpag(){
+
+//     top.location.href = 'http://127.0.0.1:5500/pagform/welpag2.html';
+// }
