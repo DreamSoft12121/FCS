5518ce597a8587c0ebeb50b44991a5f9428df83b
DylanMCDS <dylanhalo2006@gmail.com>
2023-04-17 21:58:31 -0600

    FORM TERMINADO

--- Pagina fsc/build/web/formpdf/app.js
+++ Pagina fsc/build/web/formpdf/app.js
@@ -143,15 +143,19 @@ window.addEventListener('load', async () => {
         let tel = document.getElementById('tel').value;
         let corre = document.getElementById('corre').value;
         let fechserv = document.getElementById('fechserv').value;
-        let tipserv = document.getElementById('tipserv').value;
+       
         let lugrec = document.getElementById('lugrec').value;
         let fechrec = document.getElementById('fechrec').value;
         let horarec = document.getElementById('horarec').value;
         let lugent = document.getElementById('lugent').value;
         let fechent = document.getElementById('fechent').value;
         let horaent = document.getElementById('horaent').value;
+
+        //select
+
         let sqm = document.querySelector('input[name="sqm"]:checked').value;
         let trc = document.getElementById('trc').value;
+        let tipserv = document.getElementById('tipserv').value;
         
         
         
@@ -201,7 +205,7 @@ window.addEventListener('load', async () => {
             tipcvc3, cantcvc3, altocvc3, anchocvc3, largocvc3,
             tipcvc4, cantcvc4, altocvc4, anchocvc4, largocvc4,
             tipcvc5, cantcvc5, altocvc5, anchocvc5, largocvc5,
-            tipcvc6, cantcvc6, altocvc6, anchocvc6, largocvc6);
+            tipcvc6, cantcvc6, altocvc6, anchocvc6, largocvc6, total);
     })
 
 });
@@ -212,7 +216,7 @@ async function generatePDF(rss, nom, tel, corre, fechserv, tipserv, lugrec, fech
     tipcvc3, cantcvc3, altocvc3, anchocvc3, largocvc3,
     tipcvc4, cantcvc4, altocvc4, anchocvc4, largocvc4,
     tipcvc5, cantcvc5, altocvc5, anchocvc5, largocvc5,
-    tipcvc6, cantcvc6, altocvc6, anchocvc6, largocvc6) {
+    tipcvc6, cantcvc6, altocvc6, anchocvc6, largocvc6, total) {
 
     // Desde aqui no se toca codigo
 
@@ -229,7 +233,7 @@ async function generatePDF(rss, nom, tel, corre, fechserv, tipserv, lugrec, fech
     pdf.setFontSize(7);
     pdf.text(lugrec, 180, 270); 
     pdf.text(lugent, 180, 290);   
-    pdf.setFontSize(12);
+    
     //1.Datos de contacto
     pdf.text(rss, 165, 125);
     pdf.setFontSize(10);
@@ -237,59 +241,57 @@ async function generatePDF(rss, nom, tel, corre, fechserv, tipserv, lugrec, fech
     pdf.text(tel, 165, 190);
     pdf.text(corre, 380, 190);
     //2. Correo de contacto
-    pdf.text(fechserv, 145, 456);
-    pdf.text(tipserv, 170, 475);
-
-    //if para saber que tipo de servicio eligio (local o foraneo)
-    
-    //fin del if
-    
+    pdf.text(fechserv, 145, 245);    
     pdf.text(fechrec, 355, 270);
     pdf.text(horarec, 460, 270);
-    
     pdf.text(fechent, 355, 290);
     pdf.text(horaent, 460, 290);
-    pdf.text(sqm, 170, 475);
-    pdf.text(trc, 170, 475);
+    
+    pdf.setFontSize(12);    
     //registro 1 
-    pdf.text(tipcvc1, 170, 475);
-    pdf.text(cantcvc1, 170, 475);
-    pdf.text(altocvc1, 170, 475);
-    pdf.text(anchocvc1, 170, 475);
-    pdf.text(largocvc1, 170, 475);
+    pdf.text(tipcvc1, 130, 399);
+    pdf.text(cantcvc1, 195, 399);
+    pdf.text(altocvc1, 260, 399);
+    pdf.text(anchocvc1, 325, 399);
+    pdf.text(largocvc1, 390, 399);
     //registro 2
-    pdf.text(tipcvc2, 170, 475);
-    pdf.text(cantcvc2, 170, 475);
-    pdf.text(altocvc2, 170, 475);
-    pdf.text(anchocvc2, 170, 475);
-    pdf.text(largocvc2, 170, 475);
+    pdf.text(tipcvc2, 130, 419);
+    pdf.text(cantcvc2, 195, 419);
+    pdf.text(altocvc2, 260, 419);
+    pdf.text(anchocvc2, 325, 419);
+    pdf.text(largocvc2, 390, 419);
     //registro 3
-    pdf.text(tipcvc3, 170, 475);
-    pdf.text(cantcvc3, 170, 475);
-    pdf.text(altocvc3, 170, 475);
-    pdf.text(anchocvc3, 170, 475);
-    pdf.text(largocvc3, 170, 475);
+    
+    pdf.text(tipcvc3, 130, 439);
+    pdf.text(cantcvc3, 195, 439);
+    pdf.text(altocvc3, 260, 439);
+    pdf.text(anchocvc3, 325, 439);
+    pdf.text(largocvc3, 390, 439);
     //registro 4
-    pdf.text(tipcvc4, 170, 475);
-    pdf.text(cantcvc4, 170, 475);
-    pdf.text(altocvc4, 170, 475);
-    pdf.text(anchocvc4, 170, 475);
-    pdf.text(largocvc4, 170, 475);
+    pdf.text(tipcvc4, 130, 458);
+    pdf.text(cantcvc4, 195, 458);
+    pdf.text(altocvc4, 260, 458);
+    pdf.text(anchocvc4, 325, 458);
+    pdf.text(largocvc4, 390, 458);
     //registro 5
-    pdf.text(tipcvc5, 170, 475);
-    pdf.text(cantcvc5, 170, 475);
-    pdf.text(altocvc5, 170, 475);
-    pdf.text(anchocvc5, 170, 475);
-    pdf.text(largocvc5, 170, 475);
+    pdf.text(tipcvc5, 130, 477);
+    pdf.text(cantcvc5, 195, 477);
+    pdf.text(altocvc5, 260, 477);
+    pdf.text(anchocvc5, 325, 477);
+    pdf.text(largocvc5, 390, 477);
     //registro 6
-    pdf.text(tipcvc6, 170, 475);
-    pdf.text(cantcvc6, 170, 475);
-    pdf.text(altocvc6, 170, 475);
-    pdf.text(anchocvc6, 170, 475);
-    pdf.text(largocvc6, 170, 475);
+    pdf.text(tipcvc6, 130, 497);
+    pdf.text(cantcvc6, 195, 497);
+    pdf.text(altocvc6, 260, 497);
+    pdf.text(anchocvc6, 325, 497);
+    pdf.text(largocvc6, 390, 497);
 
+    pdf.setFontSize(7);
+    pdf.text(total, 455, 512);
+    
+    
 
-    pdf.setFillColor(0,0,0);
+    //pdf.setFillColor(0,0,0);
 
     // if (parseInt(hijos) === 0) {
     //     pdf.circle(255, 374, 4, 'FD');
@@ -310,8 +312,57 @@ async function generatePDF(rss, nom, tel, corre, fechserv, tipserv, lugrec, fech
     // } else {
     //     pdf.circle(240, 718, 4, 'FD');
     // }
+    //TIPO DE SERVICIO
+    //LOCAL
+    if(parseInt(tipserv) === 1){
+        pdf.circle(437, 230, 6, 'FD');
+    }
+    //FORANEO 
+    if(parseInt(tipserv) === 2){
+        pdf.circle(437, 250, 6, 'FD')
+    }
+    //SQM
+    //NO
+    if(parseInt(sqm) === 0){
+        pdf.circle(423, 312, 6, 'FD');
+    }
+    //SI 
+    if(parseInt(sqm) === 1){
+        pdf.circle(262, 312, 6, 'FD')
+    }
+    //TRC
+    //SELECCIONAR OPC
+    if(parseInt(trc) === 0){
+        pdf.circle(0, 0, 0, 'FD');
+    }
+    //2C A 8C
+    if(parseInt(trc) === 1){
+        pdf.circle(155, 342, 6, 'FD');
+    }
+    //20C A 25C  
+    if(parseInt(trc) === 2){
+        pdf.circle(245, 342, 6, 'FD');
+    } 
+    // MENOR A 30C
+    if(parseInt(trc) === 3){
+        pdf.circle(337, 342, 6, 'FD');
+    }
+    //OTRA-NINGUNA
+    if(parseInt(trc) === 4){
+        pdf.circle(425, 342, 6, 'FD');
+    }
 
-    
+    // switch (trc){
+    //     case 1:
+    //         pdf.circle(140, 310, 4, 'FD');
+    //         break;
+    //     case 2:
+    //         pdf.circle(360, 310, 4, 'FD');
+    //         break;
+    //     case 3:
+    //         pdf.circle(460, 310, 4, 'FD');
+    //         break;        
+    // }
         // Inicio de metodo fecha
 
         const date = new Date();
