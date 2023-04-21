419cad66868cd79ad1093cc1650e8e4c455b7dc5
Dayael <guerrero.mora.dayael@gmail.com>
2023-04-20 21:36:48 -0600

    Correccion-2

--- Pagina fsc/web/paginas/login_signin.js
+++
@@ -1,8 +0,0 @@
-// const btniniciar = document.getElementById("iniciarses")
-
-// // btniniciar.addEventListener('click', linkpag)
-
-// function linkpag(){
-
-//     top.location.href = 'http://127.0.0.1:5500/pagform/welpag2.html';
-// }
